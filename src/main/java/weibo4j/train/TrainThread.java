b5289351a566a1d20e8d47c1a1dab9d9876fad65
qibaoyuan <qibaoyuan@126.com>
2014-08-18 18:38:56 +0800

    make sample available

--- src/main/java/weibo4j/train/TrainThread.java
+++ src/main/java/weibo4j/train/TrainThread.java
@@ -14,57 +14,25 @@ import java.io.*;
  * Created by yangxu on 2014/8/18.
  */
 public class TrainThread extends Thread {
-  /*  public void Sentiment() throws Exception {
 
-        InputStream  dataIn=null;
+    public void Sentimented() {
+
+        OutputStream modelOut = null;
         try {
-             dataIn = new FileInputStream("D:/sina_tweets_sentiment_category_sample/sample_0.1_5.txt");
-            ObjectStream<String> lineStream =
-                    new PlainTextByLineStream(dataIn, "UTF-8");
-            ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
-
-          //  DoccatModel model = DocumentCategorizerME.train("en", sampleStream);
-        } catch (IOException e) {
-            // Failed to read or parse training data, training failed
-            e.printStackTrace();
-        } finally {
-            if (dataIn != null) {
-                try {
-                    dataIn.close();
-                } catch (IOException e) {
-                    // Not an issue, training already finished.
-                    // The exception should be logged and investigated
-                    // if part of a production system.
-                    e.printStackTrace();
-                }
-            }
-        }
-
-    }
-
-*/
-    public void Sentimented(){
-
-        OutputStream modelOut=null;
-        try {
-            InputStream dataIn = new FileInputStream("D:/sina_tweets_sentiment_category_sample/sample_0.1_5.txt");
-            ObjectStream<String> lineStream =
-                    new PlainTextByLineStream(dataIn, "UTF-8");
+            InputStream dataIn = new FileInputStream("sample.txt");
+            ObjectStream<String> lineStream = new PlainTextByLineStream(dataIn, "UTF-8");
             ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
             DoccatModel model = DocumentCategorizerME.train("en", sampleStream);
-             modelOut = new FileOutputStream(new File("D:/demo/src/test.txt"));
+            modelOut = new FileOutputStream(new File("model.out"));
             model.serialize(modelOut);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             // Failed to save modela
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             if (modelOut != null) {
                 try {
                     modelOut.close();
-                }
-                catch (IOException e) {
+                } catch (IOException e) {
                     // Failed to correctly save model.
                     // Written model might be invalid.
                     e.printStackTrace();
