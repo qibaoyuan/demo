278d96b1c54caeac039cd69f21b78affa6a02b1f
SunLian <1549148393@qq.com>
2014-08-19 22:30:58 +0800

    covert函数在train包TrainThread中，有乱码问题

--- src/main/java/weibo4j/train/TrainThread.java
+++ src/main/java/weibo4j/train/TrainThread.java
@@ -23,7 +23,7 @@ public class TrainThread extends Thread {
             ObjectStream<String> lineStream = new PlainTextByLineStream(dataIn, "UTF-8");
             ObjectStream<DocumentSample> sampleStream = new DocumentSampleStream(lineStream);
             DoccatModel model = DocumentCategorizerME.train("en", sampleStream);
-            modelOut = new FileOutputStream(new File("model.out"));
+            modelOut = new FileOutputStream(new File("modelOut.txt"));
             model.serialize(modelOut);
         } catch (IOException e) {
             // Failed to save modela
@@ -43,14 +43,58 @@ public class TrainThread extends Thread {
 
     }
 
+    public void Covert(File rawWiboFolder,File outputFile,String catary){
+
+        try {
+            FileReader fin = new FileReader(rawWiboFolder);
+            BufferedReader reader = new BufferedReader(fin);
+            FileWriter fout = new FileWriter(outputFile,true);
+            BufferedWriter writer = new BufferedWriter(fout);
+           // PrintWriter pw = new PrintWriter(fout);
+
+           /*int c=0;
+            while((c=reader.read())!=-1){
+                char x=(char)c;
+                writer.write(x+" ");
+
+                //Character[]arrary=s.split();
+            }*/
+            String s=null;
+            while((s=reader.readLine())!=null){
+                char[]cArry=s.toCharArray();
+                writer.write(catary);
+                for(int i=0;i<cArry.length;i++){
+                    char c=cArry[i];
+                    String sappend=c+"";
+                   writer.write(" "+new String(sappend.getBytes("UTF-8"),"GBK"));
+                }
+                writer.newLine();
+            }
+            writer.close();
+            reader.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         TrainThread trainThread = new TrainThread();
-        try {
+        File f1 = new File("sample_0.1_1.txt");
+        File f2 = new File("sample_0.1_2.txt");
+        File f3 = new File("sample_0.1_3.txt");
+        File f4 = new File("sample_0.1_4.txt");
+        File f5 = new File("sampleOut.txt");
+
+        trainThread.Covert(f1,f5,"愤怒");
+        trainThread.Covert(f1,f5,"厌恶");
+        trainThread.Covert(f1,f5,"高兴");
+        trainThread.Covert(f1,f5,"低落");
+       /* try {
             //trainThread.Sentiment();
             trainThread.Sentimented();
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
 
     }
 }
